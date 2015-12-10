bd4cacf5d160c339fd48f5b3e18d3638546020ce
Martin Minka <martin.minka@gmail.com>
2015-12-10 08:22:13 +0100

    only reload nginx config instead of restart and make it less verbose restore from gziped file

--- templates/ttrss_restore.sh
+++ templates/ttrss_restore.sh
@@ -2,12 +2,11 @@
 
 #Disable site
 rm /etc/nginx/apps-enabled/ttrss
-service nginx restart
+service nginx reload > /dev/null
 
 #dump the database
-su - postgres -c "psql {{ ttrss_db_name }}" < "{{ ttrss_backup_stage }}/db.sql" >/dev/null
+su - postgres -c "pg_restore -d {{ ttrss_db_name }}" < "{{ ttrss_backup_stage }}/db.sql.gz" >/dev/null
 
 #Enable site
 ln -s /etc/nginx/apps-available/ttrss /etc/nginx/apps-enabled/ttrss
-service nginx restart
-
+service nginx reload > /dev/null
