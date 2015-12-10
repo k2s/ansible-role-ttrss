35fe646d922b8970f7b5e1903ec9ac14c6b3db77
Martin Minka <martin.minka@gmail.com>
2015-12-10 08:15:10 +0100

    only reload nginx config instead of restart and make it less verbose make sure that ttrss_backup_stage folder exists gzip backup data

--- templates/ttrss_backup.sh
+++ templates/ttrss_backup.sh
@@ -2,16 +2,15 @@
 
 #Disable site
 rm /etc/nginx/apps-enabled/ttrss
-service nginx restart
+service nginx reload > /dev/null
 
 #dump the database
-su - postgres -c "pg_dump -c {{ ttrss_db_name }}" > "{{ ttrss_backup_stage }}/db.sql"
+mkdir -p "{{ ttrss_backup_stage }}"
+su - postgres -c "pg_dump -c {{ ttrss_db_name }}" | gzip  > "{{ ttrss_backup_stage }}/db.sql.gz"
 #su - postgres -c "pg_dump -ab -T ttrss_entries -T ttrss_enclosures -T ttrss_entry_comments -T ttrss_user_entries -T ttrss_user_labels2 ttrss" > "{{ ttrss_backup_home }}/db.sql"
 #TODO: check if excluding these tables messes up refreshes
 #TODO: if I exclude them, execute an update "after_restore"
 
-
 #Enable site
 ln -s /etc/nginx/apps-available/ttrss /etc/nginx/apps-enabled/ttrss
-service nginx restart
-
+service nginx reload > /dev/null
